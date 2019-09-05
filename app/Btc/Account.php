667ccd69078303bcab65da106b221270474595c9
zhaoqi <wangzq1993@foxmail.com>
2019-09-06 05:39:04 +0800

    修改时区

--- app/Btc/Account.php
+++ app/Btc/Account.php
@@ -9,22 +9,10 @@ class Account extends HuobiBase
         parent::__construct($attributes);
     }
 
+
     /**
      * 获取账户信息
      */
-//    public function accounts()
-//    {
-//        $path = '/v1/account/accounts';
-//        $params = $this->commonParams;
-//        $sign = $this->getSign($path, $params);
-//        $params['Signature'] = $sign;
-//        $params = array_merge($params, $this->commonParams);
-////        $url = $this->buildUrl($path);
-//        $url = $this->protocl . $this->huobiUrl . $path;
-//        $result = self::sendCurl($url, $params);
-//        echo json_encode($result);
-//    }
-
     function accounts() {
         $this->api_method = "/v1/account/accounts";
         $this->req_method = 'GET';
