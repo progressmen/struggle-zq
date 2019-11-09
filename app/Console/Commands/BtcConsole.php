fa5afe59e72a8b2481452f90a21d678996b423bc
zhaoqi <wangzq1993@foxmail.com>
2019-11-09 23:25:40 +0800

    update

--- app/Console/Commands/BtcConsole.php
+++ app/Console/Commands/BtcConsole.php
@@ -39,7 +39,7 @@ class BtcConsole extends Command
      */
     public function handle()
 	{
-		// 注释
+		// 注释1
         $command = $this->argument('cmd');
         if($command == 'one'){
             $oneObj = new ActionOne();
